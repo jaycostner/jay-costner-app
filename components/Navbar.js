ef9bbf778b4f0734a1b296d36541bdd8be87da1e
James Costner <jamesmp9957@outlook.com>
2023-02-28 19:58:35 -0600

    initial upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-[#181818] flex items-center p-2 shadow-md top-0 sticky z-50 h-14">
+        <div >
+          <ul className='flex items-center justify-between text-white'>
+            <li className="mr-5">Home</li>
+            <li className="mr-5">About</li>
+            <li className="mr-5">Contact</li>
+            <li className="mr-5">Home</li>
+          </ul>
+        </div>
+    </div>
   )
 }
 
